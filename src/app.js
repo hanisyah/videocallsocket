d8068e43d960ebc50870215a31f85bbb7b434955
hanisyah <hanisyahadi4@gmail.com>
2020-09-28 12:18:47 +0700

    https

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
+let fs = require('fs');
+let perizinan = {
+    key: fs.readFileSync('key.pem'), 
+    cert: fs.readFileSync('cert.pem'), 
+    passphrase: 'hanisyah'
+};
+
 let express = require( 'express' );
 let app = express();
-let server = require( 'http' ).Server( app );
+let server = require('https').createServer(perizinan,app);
 let io = require( 'socket.io' )( server );
 let stream = require( './ws/stream' );
 let path = require( 'path' );
@@ -11,9 +18,8 @@ app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
 
 app.get( '/', ( req, res ) => {
     res.sendFile( __dirname + '/index.html' );
-} );
-
+});
 
 io.of( '/stream' ).on( 'connection', stream );
 
-server.listen( 3000 );
+server.listen(3000);
